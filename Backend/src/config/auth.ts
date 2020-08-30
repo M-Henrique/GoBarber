89f901acc0da482f90e27e411d3b24b46191a2fb
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-08-30 17:48:01 -0300

    Backend inicializado

---
+++ Backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+   jwt: {
+      secret: '0b7185c3afd3973019b9eac75ed9941e',
+      expiresIn: '1d',
+   },
+};
---
+++ Backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+   iat: number;
+   exp: number;
+   subject: string;
+}
+
+export default function ensureAuthenticated(
+   request: Request,
+   response: Response,
+   next: NextFunction
+): void {
+   const authHeader = request.headers.authorization;
+
+   if (!authHeader) throw new AppError('JWT token is missing', 401);
+
+   const [, token] = authHeader.split(' ');
+   try {
+      const decoded = verify(token, authConfig.jwt.secret);
+
+      const { subject } = decoded as TokenPayload;
+
+      request.user = { id: subject };
+
+      return next();
+   } catch {
+      throw new AppError('Invalid JWT token', 401);
+   }
+}

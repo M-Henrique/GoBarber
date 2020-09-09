30c722344141a7041dc61adfc32ac34f3ee1b8b5
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-09-09 17:48:34 -0300

    Página de cadastro finalizada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => (
    <>
-      <SignIn />
+      {/* <SignIn /> */}
+      <SignUp />
       <GlobalStyle />
    </>
 );
---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Background, Content } from './styles';
+
+const SignUp: React.FC = () => (
+   <Container>
+      <Background />
+      <Content>
+         <img src={logoImg} alt="GoBarber" />
+
+         <form>
+            <h1>Faça seu cadastro</h1>
+            <Input name="name" type="text" placeholder="Nome" icon={FiUser} />
+            <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
+            <Input name="password" type="password" placeholder="Senha" icon={FiLock} />
+            <Button type="submit">Cadastrar</Button>
+         </form>
+         <a href="">
+            <FiArrowLeft /> Voltar para logon
+         </a>
+      </Content>
+   </Container>
+);
+
+export default SignUp;
